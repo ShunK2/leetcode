ac929ad8fe0247bcbc2dd4a67544e230726dfb56
ShunK2 <robot.child0305@gmail.com>
2024-03-23 17:08:36 +0900

    first commit

---
+++ 7.ReverseInteger.py
@@ -0,0 +1,59 @@
+# 32ビット符号付き整数xが与えられたとき、整数の逆数を計算するアルゴリズムを設計してください
+# 結果が32ビット整数の範囲に収まらない場合は、0を返します
+# 例1：
+# Input: x = 123
+# Output: 321
+# 例2：
+# Input: x = -123
+# Output: -321
+# 例3：
+# Input: x = 120
+# Output: 21
+# 例4：
+# Input: x = 0
+# Output: 0
+# 例5：
+# Input: x = 1534236469
+# Output: 0
+# 例6：
+# Input: x = -2147483648
+# Output: 0
+# 制約：
+# -231 <= x <= 231 - 1
+# ==============================================================================
+# 32ビット符号付き整数の範囲は-2,147,483,648から2,147,483,647までです
+# まず、xが負の数かどうかを判定します
+# xが負の数の場合、xを正の数に変換します
+# xを文字列に変換します
+# 文字列を反転させます
+# 反転した文字列を整数に変換します
+# xが負の数の場合、整数を負の数に変換します
+# 整数が32ビット整数の範囲に収まるかどうかを判定します
+# 整数が32ビット整数の範囲に収まらない場合、0を返します
+class Solution:
+    def reverse(self, x: int) -> int:
+        max_32 = 2**31 - 1
+        if abs(x) > max_32:
+            return 0
+
+        if x < 0:
+            reverse_int = -1
+            x = x * reverse_int
+        else:
+            reverse_int = 1
+        revN = 0
+        while x > 0:
+            revN = revN * 10 + x % 10
+            x //= 10
+
+        if abs(revN) > max_32:
+            return 0
+        else:
+            return reverse_int * revN
+
+solution = Solution()
+print(solution.reverse(123)) # 321
+print(solution.reverse(-123)) # -321
+print(solution.reverse(120)) # 21
+print(solution.reverse(0)) # 0
+print(solution.reverse(1534236469)) # 0
